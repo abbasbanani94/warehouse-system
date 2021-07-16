89134fc37f98e23e74764ace0745851e9351d634
AbbasSajjad <aghulam@elatechllc.com>
2021-07-16 21:11:12 +0300

    initiate files

--- WarehouseSystem/WarehouseSystem/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WarehouseSystem
-{
-    class User
-    {
-        public static string userId = "1";
-        public static string name = "Abbas";
-        public static string role = "Admin";
-    }
-}
